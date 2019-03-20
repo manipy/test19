72cf61dbb5bc980958803826dd52257985a32a83
mani <mani.py@gmail.com>
2019-03-20 21:25:07 +0530

    new file:   pom.xml 	new file:   src/main/java/mani/mani/App.java 	new file:   src/test/java/mani/mani/AppTest.java

---
+++ src/main/java/mani/mani/App.java
@@ -0,0 +1,13 @@
+package mani.mani;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Mani test for maven and Jenkins Hello World!" );
+    }
+}
